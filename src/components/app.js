fdfc52742de32ce5c4d9b931b569f32e6f457915
unknown <zenif9@live.com>
2020-01-31 10:51:00 -0700

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Zach DeGraw Portfolio</h1>
+      </div>
+    );
+  }
+}
